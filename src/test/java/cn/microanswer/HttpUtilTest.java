f694d2da3d1524aa9c7f0aa743f1ef93352087f7
Microanswer <microanswer@outlook.com>
2019-05-12 00:50:08 +0800

    发布 maven

--- src/test/java/cn/microanswer/HttpUtilTest.java
+++ src/test/java/cn/microanswer/HttpUtilTest.java
@@ -6,6 +6,6 @@ public class HttpUtilTest {
 
     @Test
     public void test1 () throws Exception{
-        System.out.println(HttpUtil.get("https://dlr.ukb360.com/html/index3.html"));
+        System.out.println(HttpUtil.get("http://baidu.com"));
     }
 }
